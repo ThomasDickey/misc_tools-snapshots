00d8cd62ee18dd2bd16196d8635027e5cc239def
Thomas E. Dickey <dickey@invisible-island.net>
1995-02-20 13:48:21 -0500

    snapshot of project "misc_tools", label t19950220

---
+++ src/hex/hex.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define EOS '\0'
+
+int main(int argc, char **argv)
+{
+	int	n;
+
+	for (n = 1; n < argc; n++) {
+		long	value;
+		unsigned long p;
+		char	*temp;
+		char	string[80];
+
+		value = strtol(argv[n], &temp, 0);
+		string[0] = EOS;
+		for (p = value; p != 0; p >>= 8) {
+			unsigned char q = p & 0xff;
+			char last[80];
+			strcpy(last, string);
+			if (isascii(q) && isprint(q)) {
+				int	m = 0;
+				string[m++] = q;
+				if (q == '\\')
+					string[m++] = q;
+				string[m] = EOS;
+			} else {
+				sprintf(string, "\\%03o", q);
+			}
+			strcat(string, last);
+		}
+		printf("%s: %ld %#lo %#lx \"%s\"\n",
+			argv[n], value, value, value, string);
+	}
+	exit(0);
+}
