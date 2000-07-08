f3e7a15cd3345e29301abeb238c54c23c25900d1
Thomas E. Dickey <dickey@invisible-island.net>
2000-07-08 11:14:08 -0400

    snapshot of project "misc_tools", label t20000708

---
+++ src/perror/perror.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+
+int main(int argc, char *argv[])
+{
+	int n;
+	int value;
+	char *temp;
+
+	for (n = 1; n < argc; n++) {
+		temp = 0;
+		if ((value = strtol(argv[n], &temp, 0)) > 0
+		 && temp != 0
+		 && *temp == 0) {
+			errno = value;
+			perror(argv[0]);
+		}
+	}
+	return 0;
+}
