42daf867d2dbec8823e0037a6a4566e139af6303
Thomas E. Dickey <dickey@invisible-island.net>
2000-11-10 20:16:58 -0500

    snapshot of project "misc_tools", label t20001110

--- src/newpath/newpath.c
+++ src/newpath/newpath.c
@@ -1,5 +1,5 @@
 #ifndef	NO_IDENT
-static	char	Id[] = "$Header: /users/source/archives/misc_tools.vcs/src/newpath/RCS/newpath.c,v 1.8 2000/11/09 20:16:01 dickey Exp $";
+static char Id[] = "$Header: /users/source/archives/misc_tools.vcs/src/newpath/RCS/newpath.c,v 1.11 2000/11/10 20:16:58 dickey Exp $";
 #endif
 
 /*
@@ -7,20 +7,31 @@ static	char	Id[] = "$Header: /users/source/archives/misc_tools.vcs/src/newpath/R
  * Author:	T.E.Dickey
  * Created:	02 Jun 1994
  * Modified:
+ *			10 Nov 2000, port to win32
  *
- * Function:
+ * Function: (see usage message)
  */
 #include <stdlib.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 
 #ifdef WIN32
+#include <process.h>
+#else
+#include <unistd.h>
+#endif
+
+#ifdef WIN32
 #define	PATHDELIM ';'
+#define Compare(a,b) stricmp(a,b)
+#define SameDir(a,b) (!Compare((a)->nn, (b)->nn))
 #else
 #define	PATHDELIM ':'
+#define Compare(a,b) strcmp(a,b)
+#define SameDir(a,b) ((a)->sb.st_ino == (b)->sb.st_ino \
+				   && (a)->sb.st_dev == (b)->sb.st_dev)
 #endif
 
 #ifndef	TRUE
@@ -37,75 +48,93 @@ static	char	Id[] = "$Header: /users/source/archives/misc_tools.vcs/src/newpath/R
 
 #define	BLANK	""
 
-#define	TRACE(p) /* fprintf p; */
+#ifdef OPT_TRACE
+#define	TRACE(p)				fprintf p
+#else
+#define	TRACE(p)				/* fprintf p; */
+#endif
 
-typedef	struct	{
-	char		*nn;
-	struct	stat	sb;
-	} LIST;
+typedef struct
+{
+	char *nn;
+	struct stat sb;
+}
+LIST;
 
-extern	int	getopt();
-extern	int	optind;
-extern	char	*optarg;
+extern int getopt();
+extern int optind;
+extern char *optarg;
 
-static	int	allow_files;
+static int allow_files;
 
 static
-void	failed(char *s)
+void
+failed(char *s)
 {
-	perror (s);
-	exit (EXIT_FAILURE);
+	perror(s);
+	exit(EXIT_FAILURE);
 }
 
 static
-void	usage(void)
+void
+usage(void)
 {
-	static	char	*tbl[] = {
-	"Usage: newpath [options [directories]] [ - command]",
-	"",
-	"Echos the PATH environment variable with the given directories added",
-	"(the default) or removed.",
-	"",
-	"Options:",
-	"    -a NAME modify path after given NAME",
-	"    -b      put new arguments before existing path",
-	"    -d      remove duplicates/non-directory items",
-	"    -e      put new arguments after end of path",
-	"    -f      allow filenames to match, as well as directories",
-	"    -n NAME specify environment-variable to use (default: PATH)",
-	"    -p      print in C-shell form",
-	"    -r      remove arguments from path",
-	"",
-	"Put a '-' before a command which is invoked with the environment variable",
-	"updated, rather than echoing the result to standard output.",
-	0
+	static char *tbl[] =
+	{
+		"Usage: newpath [options [directories]] [ - command]",
+		"",
+		"Echos the PATH environment variable with the given directories added",
+		"(the default) or removed.",
+		"",
+		"Options:",
+		"    -a NAME modify path after given NAME",
+		"    -b      put new arguments before existing path",
+		"    -d      remove duplicates/non-directory items",
+		"    -e      put new arguments after end of path",
+		"    -f      allow filenames to match, as well as directories",
+		"    -n NAME specify environment-variable to use (default: PATH)",
+		"    -p      print in C-shell form",
+		"    -r      remove arguments from path",
+		"    -v      verbose",
+		"",
+		"Put a '-' before a command which is invoked with the environment variable",
+		"updated, rather than echoing the result to standard output.",
+		0
 	};
-	register int	j;
+	register int j;
+
 	for (j = 0; tbl[j] != 0; j++)
 		fprintf(stderr, "%s\n", tbl[j]);
 	exit(EXIT_FAILURE);
 }
 
 static
-char *	StrAlloc(char *s)	/* patch: not everyone has 'strdup()' */
+char *
+StrAlloc(char *s)				/* patch: not everyone has 'strdup()' */
 {
-	char	*d = malloc(strlen(s)+1);
+	char *d = malloc(strlen(s) + 1);
+
 	if (d == 0)
 		failed("malloc");
 	return strcpy(d, s);
 }
 
 static
-void	Remove (int offset, LIST *list, char *name)
+void
+Remove(int offset, LIST * list, char *name)
 {
-	int	n = offset;
+	int n = offset;
 
-	TRACE((stderr, "Remove %d:%s\n", offset, name))
-	while (list[n].nn != 0) {
-		if (!strcmp(list[n].nn, name)) {
-			do {
-				list[n] = list[n+1];
-			} while (list[++n].nn != 0);
+	TRACE((stderr, "Remove %d:%s\n", offset, name));
+	while (list[n].nn != 0)
+	{
+		if (!Compare(list[n].nn, name))
+		{
+			do
+			{
+				list[n] = list[n + 1];
+			}
+			while (list[++n].nn != 0);
 			n = offset - 1;
 		}
 		n++;
@@ -113,52 +142,67 @@ void	Remove (int offset, LIST *list, char *name)
 }
 
 static
-int	Append (int offset, LIST *list, char *name)
+int
+Append(int offset, LIST * list, char *name)
 {
-	int	n = offset;
-	LIST	temp, save;
+	int n = offset;
+	LIST temp, save;
 
-	TRACE((stderr, "Append %d:%s\n", offset, name))
+	TRACE((stderr, "Append %d:%s\n", offset, name));
 	temp.nn = name;
 
-	do {
+	do
+	{
 		n++;
-		save    = list[n];
+		save = list[n];
 		list[n] = temp;
-		temp    = save;
-	} while (list[n].nn != 0);
+		temp = save;
+	}
+	while (list[n].nn != 0);
 
 	return offset + 1;
 }
 
 static int
-exists(LIST *entry)
+exists(LIST * entry)
 {
-	if (stat(entry->nn, &(entry->sb)) >= 0) {
+	TRACE((stderr, "exists(%s)\n", entry->nn));
+	if (stat(entry->nn, &(entry->sb)) >= 0)
+	{
 		if ((entry->sb.st_mode & S_IFMT) == S_IFDIR)
+		{
+			TRACE((stderr, "...directory\n"));
 			return TRUE;
+		}
 		if (allow_files
-		 && (entry->sb.st_mode & S_IFMT) == S_IFREG)
-		 	return TRUE;
+			&& (entry->sb.st_mode & S_IFMT) == S_IFREG)
+		{
+			TRACE((stderr, "...file\n"));
+			return TRUE;
+		}
 	}
+	TRACE((stderr, "...not found\n"));
 	return FALSE;
 }
 
-int	main(int argc, char *argv[])
+int
+main(int argc, char *argv[])
 {
-	char	*name = "PATH";
-	char	out_delim = PATHDELIM;
-	int	remove_duplicates = FALSE;
-	size_t	length = argc;
-	int	operation = 'a';
-	char	*where = 0;
+	char *name = "PATH";
+	char out_delim = PATHDELIM;
+	int remove_duplicates = FALSE;
+	size_t length = argc;
+	int operation = 'a';
+	char *where = 0;
 
-	int	c, point = 0;
-	LIST	*list;
-	char	*s;
+	int c, point = 0;
+	LIST *list;
+	char *s;
 
-	while ((c = getopt(argc, argv, "a:bdefn:pr")) != EOF) {
-		switch (c) {
+	while ((c = getopt(argc, argv, "a:bdefn:prv")) != EOF)
+	{
+		switch (c)
+		{
 		case 'a':
 			where = optarg;
 			break;
@@ -185,6 +229,9 @@ int	main(int argc, char *argv[])
 				where = BLANK;
 			operation = c;
 			break;
+		case 'v':
+			out_delim = '\n';
+			break;
 		default:
 			usage();
 		}
@@ -195,19 +242,23 @@ int	main(int argc, char *argv[])
 	 */
 	if ((s = getenv(name)) == 0)
 		s = BLANK;
-	s = StrAlloc(s);	/* ...just in case someone else uses it */
+	s = StrAlloc(s);			/* ...just in case someone else uses it */
 	for (c = 0; s[c] != EOS; c++)
 		if (s[c] == PATHDELIM)
 			length++;
 	length += 3;
-	list = (LIST *)calloc(length, sizeof(LIST));
-	list[0].nn = BLANK;	/* dummy entry, to simplify -b option */
+	list = (LIST *) calloc(length, sizeof(LIST));
+	list[0].nn = BLANK;			/* dummy entry, to simplify -b option */
 
 	/* Split the environment variable into strings indexed in list[] */
-	for (c = 1; *s != EOS; c++) {
-		if (*s == PATHDELIM) {
+	for (c = 1; *s != EOS; c++)
+	{
+		if (*s == PATHDELIM)
+		{
 			list[c].nn = StrAlloc(".");
-		} else {
+		}
+		else
+		{
 			list[c].nn = s;
 			while (*s != PATHDELIM && *s != EOS)
 				s++;
@@ -218,45 +269,56 @@ int	main(int argc, char *argv[])
 		s++;
 	}
 	list[c].nn = 0;
-	TRACE((stderr, "%s has %d entries\n", name, c))
+	TRACE((stderr, "%s has %d entries\n", name, c));
 
 	/* Find the list-entry after which we insert/remove entries */
-	for (c = 0; list[c].nn != 0; c++) {
-		if (where != 0 && !strcmp(where, list[c].nn)) {
+	for (c = 0; list[c].nn != 0; c++)
+	{
+		if (where != 0 && !Compare(where, list[c].nn))
+		{
 			point = c;
 			break;
 		}
 	}
 	if (list[c].nn == 0)
 		point = c - 1;
-	TRACE((stderr, "argc=%d, point=%d (%c)\n", argc, point, operation))
+	TRACE((stderr, "argc=%d, point=%d (%c)\n", argc, point, operation));
 
 	/* Perform the actual insertion/removal */
-	while (optind < argc) {
+	while (optind < argc)
+	{
 		s = argv[optind++];
-		if (!strcmp(s, "-"))
+		if (!Compare(s, "-"))
 			break;
-		if (operation == 'r') {
+		if (operation == 'r')
+		{
 			Remove(point, list, s);
-		} else {
+		}
+		else
+		{
 			point = Append(point, list, s);
 		}
 	}
 
-	if (remove_duplicates) {
+	if (remove_duplicates)
+	{
 		/* Check to see if the directory exists. If not, remove it */
 		for (c = 1; list[c].nn != 0; c++)
-			if (!exists(&list[c])) {
+			if (!exists(&list[c]))
+			{
 				Remove(c, list, list[c].nn);
 				c--;
 			}
 		;
 		/* Compare the inode & device numbers of the remaining items */
-		for (c = 1; list[c].nn != 0; c++) {
-			int	d;
-			for (d = c+1; list[d].nn != 0; d++) {
-				if (list[c].sb.st_ino == list[d].sb.st_ino
-				 && list[c].sb.st_dev == list[d].sb.st_dev) {
+		for (c = 1; list[c].nn != 0; c++)
+		{
+			int d;
+
+			for (d = c + 1; list[d].nn != 0; d++)
+			{
+				if (SameDir(&list[c], &list[d]))
+				{
 					Remove(d, list, list[d].nn);
 					d--;
 				}
@@ -265,22 +327,37 @@ int	main(int argc, char *argv[])
 	}
 
 	/* Finally, print the path */
-	if (optind < argc) {
+	if (optind < argc)
+	{
 		unsigned len = strlen(name) + 2;
 		char *changed = 0;
+
 		for (c = 1; list[c].nn != 0; c++)
 			len += 1 + strlen(list[c].nn);
 		changed = malloc(len);
 		strcpy(changed, name);
-		for (c = 1; list[c].nn != 0; c++) {
+		for (c = 1; list[c].nn != 0; c++)
+		{
 			sprintf(changed + strlen(changed), "%c%s",
-				(c > 1) ? PATHDELIM : '=',
-				list[c].nn);
+					(c > 1) ? PATHDELIM : '=',
+					list[c].nn);
 		}
+		fflush(stderr);
+		fflush(stdout);
 		putenv(changed);
+#ifdef WIN32
+		spawnvp(_P_WAIT, argv[optind], &argv[optind]);
+#else
 		execvp(argv[optind], &argv[optind]);
-	} else {
-		for (c = 1; list[c].nn != 0; c++) {
+#endif
+		fflush(stderr);
+		fflush(stdout);
+	}
+	else
+	{
+		fflush(stderr);
+		for (c = 1; list[c].nn != 0; c++)
+		{
 			if (c > 1)
 				(void)putchar(out_delim);
 			(void)fputs(list[c].nn, stdout);
@@ -290,5 +367,5 @@ int	main(int argc, char *argv[])
 	}
 
 	exit(EXIT_SUCCESS);
-	/*NOTREACHED*/
+	/*NOTREACHED */
 }
