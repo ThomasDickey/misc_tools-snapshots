b20216007db56a646fbc4b9ee38561864312f2d4
Thomas E. Dickey <dickey@invisible-island.net>
2004-12-31 21:28:47 -0500

    snapshot of project "misc_tools", label t20041231

--- src/unmap/map_s.c
+++ src/unmap/map_s.c
@@ -6,7 +6,7 @@
  *		form.
  */
 
-static const char Id[] = "$Id: map_s.c,v 1.1 1997/06/10 00:03:29 tom Exp $";
+static const char Id[] = "$Id: map_s.c,v 1.4 2004/12/31 21:28:47 tom Exp $";
 
 #include "unmap.h"
 
@@ -15,76 +15,175 @@ static const char Id[] = "$Id: map_s.c,v 1.1 1997/06/10 00:03:29 tom Exp $";
 static int
 put_ch(FILE *ofp, int c)
 {
-	if (ofp != 0)
-		fputc(c, ofp);
-	return 1;
+    if (ofp != 0)
+	fputc(c, ofp);
+    return 1;
 }
 
-int map(FILE *ifp, FILE *ofp)
+/*
+ * Reference: The Unicode Standard 2.0
+ *
+ * No surrogates supported (we're storing only one 16-bit Unicode value per
+ * cell).
+ */
+static void
+utf8_outch(FILE *ofp, int ch)
 {
-	int	c;
-	int	state = 0;
-	int	count = 0;
-	int	value = 0;
+    static const unsigned byteMask = 0xBF;
+    static const unsigned otherMark = 0x80;
+    static const unsigned firstMark[] =
+    {0x00, 0x00, 0xC0, 0xE0, 0xF0, 0xF8, 0xFC};
 
-	while ((c = fgetc(ifp)) != EOF) {
-		c &= 0xff;
-		if (isprint(c)) {
-			switch (state) {
-			case 0:
-				if (c == BACKSLASH)
-					state = 1;
-				else if (c == '^')
-					state = 2;
-				else
-					count += put_ch(ofp, c);
-				break;
-			case 1:
-				state = 0;
-				switch (c) {
-				case 'E':	c = ESCAPE;	break;
-				case 'b':	c = '\b';	break;
-				case 'f':	c = '\f';	break;
-				case 'n':	c = '\n';	break;
-				case 'r':	c = '\r';	break;
-				case '^':	c = '^';	break;
-				case 't':	c = '\t';	break;
-				case '?':	c = '\177';	break;
-				default:
-					if (isoctal(c)) {
-						state = 3;
-						value = c - '0';
-					}
-				}
-				if (state == 0)
-					count += put_ch(ofp, c);
-				break;
-			case 2:
-				switch(c) {
-				case '?':	c = '\177';	break;
-				default:	c &= 0x1f;	break;
-				}
-				count += put_ch(ofp, c);
-				state = 0;
-				break;
-			case 3:
-				/* FALLTHRU */
-			case 4:
-				if (isoctal(c)) {
-					value = value * 8 + (c - '0');
-					state++;
-					break;
-				}
-				/* FALLTHRU */
-			case 5:
-				state = 0;
-				count += put_ch(ofp, value);
-				ungetc(c, ifp);
-				break;
-			}
+    int result[7], *ptr;
+    int count = 0;
+
+    if ((unsigned int) ch < 0x80)
+	count = 1;
+    else if ((unsigned int) ch < 0x800)
+	count = 2;
+    else if ((unsigned int) ch < 0x10000)
+	count = 3;
+    else if ((unsigned int) ch < 0x200000)
+	count = 4;
+    else if ((unsigned int) ch < 0x4000000)
+	count = 5;
+    else if ((unsigned int) ch <= 0x7FFFFFFF)
+	count = 6;
+    else {
+	count = 3;
+	ch = 0xFFFD;
+    }
+    ptr = result + count;
+    switch (count) {
+    case 6:
+	*--ptr = (ch | otherMark) & byteMask;
+	ch >>= 6;
+	/* FALLTHRU */
+    case 5:
+	*--ptr = (ch | otherMark) & byteMask;
+	ch >>= 6;
+	/* FALLTHRU */
+    case 4:
+	*--ptr = (ch | otherMark) & byteMask;
+	ch >>= 6;
+	/* FALLTHRU */
+    case 3:
+	*--ptr = (ch | otherMark) & byteMask;
+	ch >>= 6;
+	/* FALLTHRU */
+    case 2:
+	*--ptr = (ch | otherMark) & byteMask;
+	ch >>= 6;
+	/* FALLTHRU */
+    case 1:
+	*--ptr = (ch | firstMark[count]);
+	break;
+    }
+    while (count--)
+	put_ch(ofp, *ptr++);
+}
+
+int
+map(FILE *ifp, FILE *ofp, int utf8)
+{
+    int c;
+    int state = 0;
+    int count = 0;
+    int value = 0;
+    int digit = 0;
+    char buffer[5];
+
+    while ((c = fgetc(ifp)) != EOF) {
+	c &= 0xff;
+	if (isprint(c)) {
+	    switch (state) {
+	    case 0:
+		if (c == BACKSLASH)
+		    state = 1;
+		else if (c == '^')
+		    state = 2;
+		else
+		    count += put_ch(ofp, c);
+		break;
+	    case 1:
+		state = 0;
+		switch (c) {
+		case 'E':
+		    c = ESCAPE;
+		    break;
+		case 'b':
+		    c = '\b';
+		    break;
+		case 'f':
+		    c = '\f';
+		    break;
+		case 'n':
+		    c = '\n';
+		    break;
+		case 'r':
+		    c = '\r';
+		    break;
+		case '^':
+		    c = '^';
+		    break;
+		case 't':
+		    c = '\t';
+		    break;
+		case '?':
+		    c = '\177';
+		    break;
+		default:
+		    if (c == 'u' && utf8) {
+			state = 6;
+		    } else if (isoctal(c)) {
+			state = 3;
+			value = c - '0';
+		    }
 		}
-	}
-	if (state == 2)
+		if (state == 0)
+		    count += put_ch(ofp, c);
+		break;
+	    case 2:
+		switch (c) {
+		case '?':
+		    c = '\177';
+		    break;
+		default:
+		    c &= 0x1f;
+		    break;
+		}
+		count += put_ch(ofp, c);
+		state = 0;
+		break;
+	    case 3:
+		/* FALLTHRU */
+	    case 4:
+		if (isoctal(c)) {
+		    value = value * 8 + (c - '0');
+		    state++;
+		    break;
+		}
+		/* FALLTHRU */
+	    case 5:
+		state = 0;
 		count += put_ch(ofp, value);
-	return count;
+		ungetc(c, ifp);
+		break;
+	    case 6:
+		if (isdigit(c)) {
+		    buffer[digit++] = c;
+		    if (digit >= 4) {
+			buffer[digit] = 0;
+			sscanf(buffer, "%X", &value);
+			utf8_outch(ofp, value);
+			state = 0;
+		    }
+		}
+		break;
+	    }
+	}
+    }
+    if (state == 2)
+	count += put_ch(ofp, value);
+    return count;
 }
--- src/unmap/unmap.h
+++ src/unmap/unmap.h
@@ -1,12 +1,13 @@
-/* $Id: unmap.h,v 1.3 1997/06/10 00:56:31 tom Exp $ */
+/* $Id: unmap.h,v 1.5 2004/12/31 20:36:07 tom Exp $ */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 #include <getopt.h>
 
 #define ESCAPE '\033'
 #define BACKSLASH '\\'
 
-extern int unmap(FILE *ifp, FILE *ofp);
-extern int map(FILE *ifp, FILE *ofp);
+extern int unmap(FILE *ifp, FILE *ofp, int utf8);
+extern int map(FILE *ifp, FILE *ofp, int utf8);
